b86cbaa694045e5ab376b621d18c9d990888f399
ErdhavalR <erdhavalrathod18@gmail.com>
2022-08-06 18:11:19 +0530

    Initial Version 1 Of Data_Grid_View Management Application

---
+++ src/Screens/Main.Designer.cs
@@ -0,0 +1,315 @@
+﻿namespace GIT_Prac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle7 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.lblPersonalInfo = new System.Windows.Forms.Label();
+            this.lblInsertDetails = new System.Windows.Forms.Label();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblAddress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblZipCode = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtZipCode = new System.Windows.Forms.TextBox();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.cmbCity = new System.Windows.Forms.ComboBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.dgvPersonalDetails = new System.Windows.Forms.DataGridView();
+            this.clmSrNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmAddress = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmCity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.clmZipCode = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonalDetails)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblPersonalInfo
+            // 
+            this.lblPersonalInfo.AutoSize = true;
+            this.lblPersonalInfo.Font = new System.Drawing.Font("Lucida Bright", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPersonalInfo.Location = new System.Drawing.Point(234, 9);
+            this.lblPersonalInfo.Name = "lblPersonalInfo";
+            this.lblPersonalInfo.Size = new System.Drawing.Size(192, 31);
+            this.lblPersonalInfo.TabIndex = 0;
+            this.lblPersonalInfo.Text = "Personal Info";
+            // 
+            // lblInsertDetails
+            // 
+            this.lblInsertDetails.AutoSize = true;
+            this.lblInsertDetails.Font = new System.Drawing.Font("Lucida Bright", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInsertDetails.Location = new System.Drawing.Point(12, 54);
+            this.lblInsertDetails.Name = "lblInsertDetails";
+            this.lblInsertDetails.Size = new System.Drawing.Size(268, 24);
+            this.lblInsertDetails.TabIndex = 1;
+            this.lblInsertDetails.Text = "Insert Following Details";
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblName.Location = new System.Drawing.Point(26, 112);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(64, 22);
+            this.lblName.TabIndex = 2;
+            this.lblName.Text = "Name";
+            // 
+            // lblAddress
+            // 
+            this.lblAddress.AutoSize = true;
+            this.lblAddress.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAddress.Location = new System.Drawing.Point(335, 112);
+            this.lblAddress.Name = "lblAddress";
+            this.lblAddress.Size = new System.Drawing.Size(91, 22);
+            this.lblAddress.TabIndex = 3;
+            this.lblAddress.Text = "Address";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(26, 169);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(49, 22);
+            this.lblCity.TabIndex = 4;
+            this.lblCity.Text = "City";
+            // 
+            // lblZipCode
+            // 
+            this.lblZipCode.AutoSize = true;
+            this.lblZipCode.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblZipCode.Location = new System.Drawing.Point(325, 169);
+            this.lblZipCode.Name = "lblZipCode";
+            this.lblZipCode.Size = new System.Drawing.Size(101, 22);
+            this.lblZipCode.TabIndex = 5;
+            this.lblZipCode.Text = "Zip_Code";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtName.Location = new System.Drawing.Point(96, 109);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(201, 30);
+            this.txtName.TabIndex = 6;
+            // 
+            // txtZipCode
+            // 
+            this.txtZipCode.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtZipCode.Location = new System.Drawing.Point(432, 166);
+            this.txtZipCode.MaxLength = 6;
+            this.txtZipCode.Name = "txtZipCode";
+            this.txtZipCode.Size = new System.Drawing.Size(201, 30);
+            this.txtZipCode.TabIndex = 7;
+            this.txtZipCode.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtZipCode_KeyPress);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtAddress.Location = new System.Drawing.Point(432, 112);
+            this.txtAddress.Multiline = true;
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(201, 44);
+            this.txtAddress.TabIndex = 8;
+            // 
+            // cmbCity
+            // 
+            this.cmbCity.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbCity.FormattingEnabled = true;
+            this.cmbCity.Items.AddRange(new object[] {
+            "None",
+            "Rajkot",
+            "Surat",
+            "Vadodara",
+            "Ahemadabad",
+            "Gandhinagar"});
+            this.cmbCity.Location = new System.Drawing.Point(97, 166);
+            this.cmbCity.Name = "cmbCity";
+            this.cmbCity.Size = new System.Drawing.Size(201, 30);
+            this.cmbCity.TabIndex = 9;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(41, 223);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(114, 47);
+            this.btnAdd.TabIndex = 10;
+            this.btnAdd.Text = "ADD";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(197, 223);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(114, 47);
+            this.btnUpdate.TabIndex = 11;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(350, 223);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(114, 47);
+            this.btnSave.TabIndex = 12;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Lucida Bright", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(503, 223);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(114, 47);
+            this.btnDelete.TabIndex = 13;
+            this.btnDelete.Text = "DELETE";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // dgvPersonalDetails
+            // 
+            this.dgvPersonalDetails.AllowUserToAddRows = false;
+            this.dgvPersonalDetails.AllowUserToResizeColumns = false;
+            this.dgvPersonalDetails.AllowUserToResizeRows = false;
+            this.dgvPersonalDetails.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridViewCellStyle7.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle7.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle7.Font = new System.Drawing.Font("Arial Unicode MS", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle7.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle7.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle7.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle7.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgvPersonalDetails.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle7;
+            this.dgvPersonalDetails.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvPersonalDetails.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clmSrNo,
+            this.clmName,
+            this.clmAddress,
+            this.clmCity,
+            this.clmZipCode});
+            this.dgvPersonalDetails.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dgvPersonalDetails.Location = new System.Drawing.Point(13, 288);
+            this.dgvPersonalDetails.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvPersonalDetails.Name = "dgvPersonalDetails";
+            this.dgvPersonalDetails.RowHeadersVisible = false;
+            this.dgvPersonalDetails.Size = new System.Drawing.Size(632, 232);
+            this.dgvPersonalDetails.TabIndex = 14;
+            this.dgvPersonalDetails.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.dgvPersonalDetails_CellFormatting);
+            // 
+            // clmSrNo
+            // 
+            this.clmSrNo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.clmSrNo.HeaderText = "Sr.No";
+            this.clmSrNo.Name = "clmSrNo";
+            this.clmSrNo.Width = 60;
+            // 
+            // clmName
+            // 
+            this.clmName.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmName.HeaderText = "Name";
+            this.clmName.Name = "clmName";
+            // 
+            // clmAddress
+            // 
+            this.clmAddress.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmAddress.HeaderText = "Address";
+            this.clmAddress.Name = "clmAddress";
+            this.clmAddress.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            // 
+            // clmCity
+            // 
+            this.clmCity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmCity.HeaderText = "City";
+            this.clmCity.Name = "clmCity";
+            // 
+            // clmZipCode
+            // 
+            this.clmZipCode.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.clmZipCode.HeaderText = "Zip_Code";
+            this.clmZipCode.Name = "clmZipCode";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 533);
+            this.Controls.Add(this.dgvPersonalDetails);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.cmbCity);
+            this.Controls.Add(this.txtAddress);
+            this.Controls.Add(this.txtZipCode);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblZipCode);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblAddress);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.lblInsertDetails);
+            this.Controls.Add(this.lblPersonalInfo);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Home_Page";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvPersonalDetails)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblPersonalInfo;
+        private System.Windows.Forms.Label lblInsertDetails;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblAddress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblZipCode;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtZipCode;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.ComboBox cmbCity;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.DataGridView dgvPersonalDetails;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmSrNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmAddress;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmCity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clmZipCode;
+    }
+}
+
---
+++ src/Screens/Main.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace GIT_Prac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        #region // ------------------------------ TextBox ZipCode KeyPress Event ------------------------------ //
+        private void txtZipCode_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        #endregion
+
+        #region // ------------------------------ Data_Grid_View_Personal_Details Cell_Formatting Event ------------------------------ //
+        private void dgvPersonalDetails_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            int Index = 0;
+            foreach (DataGridViewRow Row in dgvPersonalDetails.Rows)
+            {
+                Row.Cells[Index].Value = Convert.ToString(Row.Index + 1);
+            }
+        }
+        #endregion
+
+        #region // ------------------------------ Button Add Click Event ------------------------------ //
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (txtName.Text == "" || txtAddress.Text == "" || txtZipCode.Text == "" || cmbCity.Text == "")
+            {
+                MessageBox.Show("Please Fill All The Fields");
+            }
+            else
+            {
+                string s = txtAddress.Text;
+                RegexOptions OP = RegexOptions.None;
+                Regex Reg = new Regex("[\r]{1}");
+                s = Reg.Replace(s, " ");
+                txtAddress.Text = s;
+                dgvPersonalDetails.Rows.Add("", txtName.Text, txtAddress.Text, cmbCity.SelectedItem, txtZipCode.Text);
+                MessageBox.Show("Record Added Successful");
+                txtName.Clear();
+                cmbCity.SelectedIndex = 0;
+                txtAddress.Clear();
+                txtZipCode.Clear();
+            }
+        }
+        #endregion
+
+        #region // ------------------------------ Main_Form Load Event ------------------------------ //
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            cmbCity.SelectedIndex = 0;
+        }
+        #endregion
+
+    }
+}
